75f2b6f8f8f38be09c8257d945d343d3d21448cf
Kirka <kirill.kobzev5555@gmail.com>
2023-02-01 00:00:31 +0300

    fix task_8 (task_8)

--- task_8/8.py
+++ task_8/8.py
@@ -1,6 +1,6 @@
 from itertools import product
-words = product('дкмо', repeat=5)
+words = product('клрт', repeat=4)
 k = []
 for i in words:
 	k.append(''.join(i))
-print(k.index('комод') - k.index('домок') + 1)
+print(k[66])
