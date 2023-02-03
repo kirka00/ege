86cee817ec7eedb34dc9c0e7c3f1a17896eb4342
Kirka <kirill.kobzev5555@gmail.com>
2023-02-03 22:26:18 +0300

    fix 5.pyy

--- task_17/5.py
+++ task_17/5.py
@@ -1,12 +1,12 @@
 with open('task_17/files/17-304.txt') as f:
-    s = [int(x) for x in f]
-    n = []
-    for i in range(len(s) - 1):
-        if (sum([int(j) for j in str(s[i])[0::2]]) \
-			> sum([int(j) for j in str(s[i])[1::2]]) \
-				and sum([int(j) for j in str(s[i + 1])[0::2]]) > \
-					sum([int(j) for j in str(s[i + 1])[1::2]])) \
-						and (abs(s[i] + s[i + 1]) % \
-							(min([q for q in s[0::2]]))== 0):
-            n.append(s[i] + s[i + 1])
+	s = [int(i) for i in f]
+	n = []
+	for i in range(len(s) - 1):
+		if sum([int(j) for j in str(s[i]) if int(j) % 2 != 0]) > sum([int(j)
+			for j in str(s[i]) if int(j) % 2 == 0]) and \
+				sum([int(j) for j in str(s[i + 1]) if int(j) % 2 != 0]) > \
+					sum([int(j) for j in str(s[i + 1]) \
+						if int(j) % 2 == 0]) and (s[i] + s[i + 1]) % \
+								min([i for i in s if i % 2 != 0]) == 0:
+			n.append(s[i] + s[i + 1])
 print(len(n), min(n))
