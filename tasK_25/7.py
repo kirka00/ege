573c1cd1e11f29fd10b83a09c253d351f24c1c64
Kirka <kirill.kobzev5555@gmail.com>
2023-02-25 08:59:59 +0300

    fix bugs in task_25

--- tasK_25/7.py
+++ tasK_25/7.py
@@ -1,22 +1,26 @@
-def f(n):
+def prost(n):
 	for d in range(2, round(n ** 0.5) + 1):
 		if n % d == 0:
 			return False
 	return True
 
 
+primes = set()
+for i in range(3, int(63_000_000**0.25) + 1):
+    if prost(i):
+        primes.add(i)
+
 for i in range(63_000_000, 75_000_001):
 	x = i
 	while x % 2 == 0:
 		x //= 2
-	q4 = round(x ** 0.25)
-	if f(q4) and q4 ** 4 == x:
+	q4 = int(x ** 0.25)
+	if q4 in primes and q4 ** 4 == x:
 		print(i, x)
 
 
 '''
 63123848 7890481
 66724352 130321
-67108864 1
 71639296 279841
 '''
--- tasK_25/9.py
+++ tasK_25/9.py
@@ -1,24 +1,21 @@
 i = 650_001
 k = 0
-while k != 7:
+while k != 4:
 	divs = set()
 	for d in range(2, round(i ** 0.5) + 1):
 		if i % d == 0:
 			divs.add(d)
 			divs.add(i // d)
-	if len(divs) != 0:
-		if sum(divs) % 37 == 23:
+	if len(divs):
+		if sum(divs) // len(divs) % 37 == 23:
 			k += 1
-			print(i, sum(divs))
+			print(i, sum(divs) // len(divs))
 	i += 1
 
 
 '''
-650031 237008
-650130 1031213
-650160 1968719
-650187 366212
-650210 687261
-650298 801221
-650304 1215325
+650055 19189
+650062 34618
+650089 11123
+650090 24295
 '''
