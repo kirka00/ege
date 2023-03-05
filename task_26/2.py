85cf25341a5386663df6ce2f595d8642f10ff91b
Kirka <kirill.kobzev5555@gmail.com>
2023-03-05 14:11:00 +0300

    fix tasks in task_26

--- task_26/2.py
+++ task_26/2.py
@@ -1,4 +1,11 @@
 with open('files/26-k4.txt') as f:
 	n, k = map(int, f.readline().split())
+	print(k)
 	data = sorted([int(i) for i in f], reverse=True)
-	print(sum(data[k:k+1]) // k, sum(data[:k]) // k)
+	print(sum(data[k:k+k]) // k, sum(data[:k]) // k)
+
+
+
+'''
+856 953
+'''
