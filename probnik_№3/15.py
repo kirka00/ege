f103fa1c267121f8b512577e6407d9aea539088d
Kirka <kirill.kobzev5555@gmail.com>
2023-03-15 00:04:48 +0300

    fix task_15

--- probnik_№3/15.py
+++ probnik_№3/15.py
@@ -1,12 +1,1 @@
-p = set(range(15, 31))
-q = set(range(5, 61))
-a = set()
-for x in range(-1000, 1000):
-	if not (not ((x in q)) or (x in p) and (x in a)):
-		a.add(x)
-print(a)
-print(len(a))
-
-
-
-# 55
+# 30 (алгебра логики на листе бумаги)
